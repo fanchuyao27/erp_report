90ebef8360a80dc309426740566d30bf845d47ab
fanchuyao27 <92429920+fanchuyao27@users.noreply.github.com>
2022-11-18 16:53:30 +0800

    Update DataType.java

--- utils/DataType.java
+++ utils/DataType.java
@@ -1,12 +1,5 @@
 package com.sfp.utils;
 
-/**
- * @author xl
- * @version 1.0.0
- * @ClassName DataType.java
- * @Description TODO
- * @createTime 2022年07月21日 16:35:00
- */
 public enum DataType {
     Top60, Supplier, Overview, MonthlyPrice, LargeAmount, Top20;
 }
